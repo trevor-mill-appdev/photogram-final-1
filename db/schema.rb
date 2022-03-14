f03257c1000c3a83f62ca05284c3e0e3da9cdebe
trevor-mill <trevor.million@chicagobooth.edu>
2022-03-14 17:23:47 +0000

    likes table works

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_14_172203) do
+ActiveRecord::Schema.define(version: 2022_03_14_172305) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "author_id"
@@ -32,6 +32,13 @@ ActiveRecord::Schema.define(version: 2022_03_14_172203) do
     t.integer "recipient_id"
     t.integer "sender_id"
     t.string "status"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.integer "fan_id"
+    t.integer "photo_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
