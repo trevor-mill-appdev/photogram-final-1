83d3d95537bb3e2bab8bd1165f7cdd4694d60a40
trevor-mill <trevor.million@chicagobooth.edu>
2022-03-14 17:43:46 +0000

    associations built

--- app/models/follow_request.rb
+++ app/models/follow_request.rb
@@ -1,2 +1,17 @@
+# == Schema Information
+#
+# Table name: follow_requests
+#
+#  id           :integer          not null, primary key
+#  status       :string
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#  recipient_id :integer
+#  sender_id    :integer
+#
 class FollowRequest < ApplicationRecord
+
+  belongs_to(:recipient, { :required => true, :class_name => "User", :foreign_key => "recipient_id" })
+
+  belongs_to(:sender, { :required => true, :class_name => "User", :foreign_key => "sender_id" })
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,2 +1,16 @@
+# == Schema Information
+#
+# Table name: likes
+#
+#  id         :integer          not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  fan_id     :integer
+#  photo_id   :integer
+#
 class Like < ApplicationRecord
+
+  belongs_to(:post, { :required => true, :class_name => "Photo", :foreign_key => "photo_id", :counter_cache => true })
+
+  belongs_to(:fan, { :required => true, :class_name => "User", :foreign_key => "fan_id", :counter_cache => true })
 end
