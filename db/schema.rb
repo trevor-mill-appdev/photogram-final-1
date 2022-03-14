fcfd8f79e2b3fa7e3e9e9402abce2e342324c6d5
trevor-mill <trevor.million@chicagobooth.edu>
2022-03-14 17:12:27 +0000

    user table works

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_14_170952) do
+ActiveRecord::Schema.define(version: 2022_03_14_171135) do
 
   create_table "photos", force: :cascade do |t|
     t.text "caption"
     t.string "image"
     t.integer "owner_id"
+    t.integer "likes_count"
+    t.integer "comments_count"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
+    t.boolean "private"
+    t.string "username"
     t.integer "likes_count"
     t.integer "comments_count"
     t.datetime "created_at", precision: 6, null: false
